7845d5d56a623aaf9bc3b47c1a7121fef3424b72
shahrozraza1 <shahrozraza57@gmail.com>
2023-07-10 15:47:33 +0500

    Added-helper-Kraken-Value

--- repositories/apirepository/helper.go
+++ repositories/apirepository/helper.go
@@ -3,7 +3,13 @@ package apirepository
 func GetKrakenPriceBTC(resp map[string]interface{}) string {
 	if results, found := resp["result"].(map[string]interface{}); found {
 		if xbtusdt, ok := results["XBTUSDT"].(map[string]interface{}); ok {
-			return xbtusdt["o"].(string)
+			if a, ok := xbtusdt["a"].([]interface{}); ok {
+				if len(a) > 0 {
+					if price, ok := a[0].(string); ok {
+						return price
+					}
+				}
+			}
 		}
 	} else {
 		//handle error - the map didn't contain this key
@@ -14,7 +20,13 @@ func GetKrakenPriceBTC(resp map[string]interface{}) string {
 func GetKrakenETH(resp map[string]interface{}) string {
 	if results, found := resp["result"].(map[string]interface{}); found {
 		if ethusdt, ok := results["ETHUSDT"].(map[string]interface{}); ok {
-			return ethusdt["o"].(string)
+			if a, ok := ethusdt["a"].([]interface{}); ok {
+				if len(a) > 0 {
+					if price, ok := a[0].(string); ok {
+						return price
+					}
+				}
+			}
 		}
 	} else {
 		//handle error - the map didn't contain this key
