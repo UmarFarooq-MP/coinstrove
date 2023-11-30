74b4a8f7ef87ff0ea09be4ba34b9f1823686c93d
UmarFarooq-MP <umarfarooq.waheed@gmail.com>
2023-11-30 18:44:57 +0500

    new-image-tag-latest

--- build/git_update.sh
+++ build/git_update.sh
@@ -1,24 +1,6 @@
 #!/bin/bash
 
-VERSION=""
-
-# get parameters
-while getopts v: flag
-do
-  case "${flag}" in
-    v) VERSION=${OPTARG};;
-  esac
-done
-
-# get highest tag number, and add v0.1.0 if doesn't exist
-git fetch --prune --unshallow 2>/dev/null
-CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
-
-if [[ $CURRENT_VERSION == '' ]]
-then
-  CURRENT_VERSION='v0.1.0'
-fi
-echo "Current Version: $CURRENT_VERSION"
+# ... (existing code)
 
 # replace . with space so can split into an array
 CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
@@ -28,23 +10,9 @@ VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION == 'major' ]]
-then
-  VNUM1=v$((VNUM1+1))
-elif [[ $VERSION == 'minor' ]]
-then
-  VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
-then
-  VNUM3=$((VNUM3+1))
-else
-  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
-  exit 1
-fi
-
-# create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
+# Always set the tag to "latest"
+NEW_TAG="latest"
+echo "Updating to $NEW_TAG"
 
 # get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
